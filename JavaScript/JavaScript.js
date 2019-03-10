802890f7fca37af81d05ca16aea93f59eb2a4664
mohamed darwesh <mohamed_darwesh123@hotmail.com>
2019-03-10 21:03:08 +0200

    Adding Files

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+
+    $('#btnegypt').click(function () {
+
+        $('body').css({ 'background': 'url("Photo/Egypt.jpg") no-repeat center fixed', 'background-size': 'cover' });
+        $('.egypt').show();
+        $('.italy').hide();
+        $('.germany').hide();
+    });
+
+
+
+    $('#btnitaly').click(function () {
+
+        $('body').css({ 'background': 'url("Photo/italy.jpg") no-repeat center fixed', 'background-size': 'cover' });
+        $('.egypt').hide();
+        $('.italy').show();
+        $('.germany').hide();
+    });
+
+
+
+
+    $('#btngermany').click(function () {
+
+        $('body').css({ 'background': 'url("Photo/germany.jpg") no-repeat center fixed', 'background-size': 'cover' });
+        $('.egypt').hide();
+        $('.italy').hide();
+        $('.germany').show();
+    });
+});
